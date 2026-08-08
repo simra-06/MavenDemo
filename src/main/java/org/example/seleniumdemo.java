9a21a33c7b49752cb08c016f543aae3686c9b7ba
Simra Khan <simrakcolumbian.9c30@gmail.com>
2026-08-08 12:43:35 +0530

    Initial Commit

---
+++ src/main/java/org/example/seleniumdemo.java
@@ -0,0 +1,18 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class seleniumdemo {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.github.com");
+        System.out.println("Page Title: " + driver.getTitle());
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+}
