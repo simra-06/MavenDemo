f87dbb8b1418e01034940494864c44fa7434cb5d
Simra Khan <simrakcolumbian.9c30@gmail.com>
2026-08-08 13:14:14 +0530

    Update Selenium Maven project

--- src/main/java/org/example/SeleniumDemo.java
+++ src/main/java/org/example/SeleniumDemo.java
@@ -1,7 +1,7 @@
 package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class seleniumdemo {
+public class SeleniumDemo {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
